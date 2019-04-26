4285ceed5d3bc806e4aef5edbe2a2ab2f1cb0eda
= <julianstampfli4@gmail.com>
2019-04-26 02:38:14 +0200

    added files type

--- src/tests.py
+++ src/tests.py
@@ -1,27 +1,11 @@
 import pytsk3
-img_info = pytsk3.Img_Info("/dev/nvme0n1p5")
-print(img_info)
-fs_info = pytsk3.FS_Info(img_info, offset=0)
-print(fs_info.info)
-fs_info.info
-
-root = fs_info.open_dir("/")
-for i in root:
-    print(i.info.name.name)
-    print(i.info.name.type)
-    if i.info.name.type==pytsk3.TSK_FS_NAME_TYPE_DIR:
-        print("directory!")
-    elif i.info.name.type == pytsk3.TSK_FS_NAME_TYPE_REG:
-        print("I'm a file")
-        curDir = i.open_dir()
-        for j in curDir:
-                print(j.info.name.name)
-                print(j.info.name.type)
-
-
 
 def open_directory_rec(path):
-        curDir = fs_info.open_dir(path)
+        try:
+                curDir = fs_info.open_dir(path)
+        except:
+                print("exception!")
+                return
         for element in curDir:
                 print(path)
                 print(element.info.name.name)
@@ -34,3 +18,18 @@ def open_directory_rec(path):
                 elif element.info.name.type == pytsk3.TSK_FS_NAME_TYPE_REG:
                         print("I'm a file")
 
+img_info = pytsk3.Img_Info("/dev/nvme0n1p5")
+fs_info = pytsk3.FS_Info(img_info, offset=0)
+fs_info.info
+
+open_directory_rec("/home/")
+
+pytsk3.TSK_FS_META_MODE_IRUSR.__str__()
+pytsk3.TSK_FS_META_MODE_IRGRP.__str__()
+pytsk3.TSK_FS_META_MODE_IROTH.__str__()
+pytsk3.TSK_FS_META_MODE_IWUSR.__str__()
+pytsk3.TSK_FS_META_MODE_IWGRP.__str__()
+pytsk3.TSK_FS_META_MODE_IWOTH.__str__()
+pytsk3.TSK_FS_META_MODE_IXUSR.__str__()
+pytsk3.TSK_FS_META_MODE_IXGRP.__str__()
+pytsk3.TSK_FS_META_MODE_IXOTH.__str__()
