f4b870297273c8b8dae88b3ab22d7fe72558174e
Julian Stampfli <julian.stampfli@spoud.io>
2019-05-01 17:44:48 +0200

    extended config settings

--- src/config/config.py
+++ src/config/config.py
@@ -1,1 +1,11 @@
 import yaml
+from fids_config import FidsConfig
+from db_config import DbConfig
+
+
+class Config:
+    def __init__(self, config_file='config.yaml'):
+        with open(config_file, 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+        self.fids_config = FidsConfig(cfg['fids'])
+        self.db_config = DbConfig(cfg['db'])
---
+++ src/config/db_config.py
@@ -0,0 +1,7 @@
+class DbConfig:
+    def __init__(self, config):
+        self.host = config.host
+        self.port = config.port
+        self.dbname = config.dbname
+        self.user = config.user
+        self.password = config.password
