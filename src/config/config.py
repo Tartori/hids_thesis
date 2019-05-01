c6690d9f9438b873055012c45b50321d5882ca56
= <julianstampfli4@gmail.com>
2019-05-02 01:13:38 +0200

    added database functionality

--- src/config/config.py
+++ src/config/config.py
@@ -1,11 +1,12 @@
 import yaml
-from fids_config import FidsConfig
-from db_config import DbConfig
+from .fids_config import FidsConfig
+from .db_config import DbConfig
 
 
 class Config:
-    def __init__(self, config_file='config.yaml'):
+    def __init__(self, config_file='./src/config.yaml'):
         with open(config_file, 'r') as ymlfile:
             cfg = yaml.load(ymlfile)
+        print(cfg)
         self.fids_config = FidsConfig(cfg['fids'])
         self.db_config = DbConfig(cfg['db'])
--- src/fids.py
+++ src/fids.py
@@ -1,13 +1,20 @@
 from scaner import Scanner
 from hids_file import HidsFile
+from config.config import Config
+from db_connector.db import Database
 
 
 class FIDS:
     def scan_system(self):
-        scanner = Scanner(image_path="../images/usb1.dd", path="/")
+        config = Config()
+        scanner = Scanner(fids_config=config.fids_config)
         files = scanner.scan()
         print(files)
+        db = Database(config.db_config)
+        for file in files:
+            db.safeFile(file)
+        db.commit()
 
 
 if __name__ == "__main__":
-    pass
+    FIDS().scan_system()
